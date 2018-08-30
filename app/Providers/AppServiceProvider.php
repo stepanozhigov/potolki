375764f10ae8a1be79634632e314cb47400fbbdc
root <root@89-108-103-224.cloudvps.regruhosting.ru>
2018-08-30 16:16:29 +0300

    init voyager

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,14 +3,18 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use TCG\Voyager\Http\Controllers\ContentTypes\Image as tcgImage;
+use App\Http\Controllers\Voyager\Image as tcgAppImage;
 class AppServiceProvider extends ServiceProvider
 {
+    
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
+
+
     public function boot()
     {
         //
@@ -23,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        //$this->app->bind('TCG\Voyager\Http\Controllers\ContentTypes\Image1', 'App\Http\Controllers\Voyager\Image');
+        //dd($this->app->bind('TCG\VoyageentTypes\Image', 'App\Http\Controllers\Voyager\Image'));
+        $this->app->bind(VoyagerBreadController::class, MyBreadController::class);
     }
 }
