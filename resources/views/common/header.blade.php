ec5279aa7a9db3e54e5235fade5d1e41d701ab32
root <root@89-108-103-224.cloudvps.regruhosting.ru>
2018-08-31 18:10:44 +0300

    windows main page refactoring

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,5 +1,5 @@
-<header class="header container">
-    <div class="header__top">
+<header class="header">
+    <div class="header__top container">
         <a href="/" class="header_logo logo">
             <img class="logo__icon" src="/img/logo-icon.svg" alt="логотип Твой Стиль">
             <img class="logo__text" src="/img/logo-text.svg" alt="графема Твой Стиль">        
@@ -11,11 +11,30 @@
                 <a href="{{ $direction->site }}" class="dropdown__item">{{ $direction->name }}</a>
             @endforeach
         </div>
+
+        <nav class="menu header__menu">
+            <div class="dropdown menu__item menu__item_dropdown">
+                <a href="/asd" class="menu__item">Калькулятор</a>
+            </div>
+            <div class="dropdown menu__item menu__item_dropdown">
+                <a href="/asd" class="menu__item">Услуги</a>
+            </div>
+            <a href="/asd" class="menu__item">Скидки</a>
+            <a href="/asd" class="menu__item">Фото</a>
+            <div class="dropdown menu__item menu__item_dropdown">
+                <a href="/asd" class="menu__item">Информация</a>
+            </div>
+        </nav>
     </div>
-    <div class="header__bottom">
+    <hr class="header__line">
+    <div class="header__bottom container">
         <div class="header__offices dropdown">
             <a class="dropdown__item dropdown__item_active"><i class="arrow arrow_down dropdown__arrow"></i> Владивосток, Запорожская, 77</a>
-
+        </div>
+        <div class="header__feedbacks">
+            <a href="" class="phone header__phone">8 800 333-97-14</a>
+            <a href="" class="whatsapp header__whatsapp">Написать в WhatsApp</a>
+            <a href="" class="header__callback">Заказать звонок</a>
         </div>
     </div>    
 </header>
--- resources/views/windows/main.blade.php
+++ resources/views/windows/main.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     @include('common.header')
 
-    <section class="main-offer main-offer_center">
-        <h1 class="title title_center main-offer__title">Фабрика пластиковых окон «Твой&nbspстиль»</h1>
+    <section class="main-offer">
+        <h1 class="title main-offer__title">Фабрика пластиковых окон «Твой&nbspстиль»</h1>
         <p class="main-offer__subtitle subtitle">Выберите город, чтобы перейти на сайт</p>
         <a href="/" class="button button_red main-offer__button  ">Выбрать Сочи</a>
         <a href="/" class="button button_gray main-offer__button  ">Выбрать другой город</a>
