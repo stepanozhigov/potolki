9dd8417af4b2ed5c8a72efd753813a20091578ad
root <root@89-108-103-224.cloudvps.regruhosting.ru>
2018-09-03 12:22:00 +0300

    main-offer up

--- resources/views/windows/main.blade.php
+++ resources/views/windows/main.blade.php
@@ -4,10 +4,21 @@
     @include('common.header')
 
     <section class="main-offer">
-        <h1 class="title main-offer__title">Фабрика пластиковых окон «Твой&nbspстиль»</h1>
-        <p class="main-offer__subtitle subtitle">Выберите город, чтобы перейти на сайт</p>
-        <a href="/" class="button button_red main-offer__button  ">Выбрать Сочи</a>
-        <a href="/" class="button button_gray main-offer__button  ">Выбрать другой город</a>
+        <div class="marin-offer__inner container">
+            <p class="overtitle main-offer__overtitle">Уникальное предложение</p>
+            <p class="title main-offer__title">Пластиковые окна со скидкой 30%</p>
+            <p class="main-offer__subtitle subtitle">Для получения скидки оставьте заявку</p>
+
+            <form class="form" action="">
+                <input class="input main-offer__input" type="text"  placeholder="Ваше имя">
+                <input class="input main-offer__input" type="tel"  placeholder="Ваш телефон">
+
+                <button class="button button_red">Вызвать замерщика</button>
+
+                <p class="form__agreement">Оставляя контактную информацию, Вы соглашаетесь на обработку персональных данных</p>
+            </form>
+        </div>
+        
     </section>
 
     
