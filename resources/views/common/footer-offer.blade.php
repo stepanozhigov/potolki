ba4c48b9f28dda07a6f6a42186f19600f4c62f36
root <root@89-108-103-224.cloudvps.regruhosting.ru>
2018-09-18 17:27:01 +0300

    footer ref

--- resources/views/common/footer-offer.blade.php
+++ resources/views/common/footer-offer.blade.php
@@ -2,6 +2,6 @@
     <div class="container footer-offer__content">
         <h2 class="card-title footer-offer__title">Запишитесь на замер <span>сегодня</span> и получите <br> дополнительную <span>скидку 10%</span></h2>
         <p class="intro footer-offer__intro">Оставьте контактные данные и наш специалист перезвонит Вам <span> в течении 5 минут</span></p>
-        @include('common.gui.form', [])
+        @include('common.gui.form', ['class' => 'footer-offer__form'])
     </div>
 </section>
--- resources/views/common/gui/form.blade.php
+++ resources/views/common/gui/form.blade.php
@@ -1,6 +1,6 @@
 <form class="form {{ $class ?? ''}}" action="">
     <input class="input form__input" type="text" placeholder="Ваше имя">
     <input class="input form__input" type="tel" placeholder="Ваш телефон">
-    <button class="button button_red">Вызвать замерщика</button>
+    <button class="button button_red form__button">Вызвать замерщика</button>
     <p class="subtext form__agreement">Оставляя контактную информацию, Вы соглашаетесь на обработку персональных данных</p>
 </form>
