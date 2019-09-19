f4944e1a62c866db2d5d53be2c1127071cf0b79a
root <root@potolki-ts.ru>
2019-09-19 11:44:48 +0300

    Добавил счетчик

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -110,5 +110,14 @@
     }
 </script>
 
+<script>
+(function(w, d, s, h, id) {
+    w.roistatProjectId = id; w.roistatHost = h;
+    var p = d.location.protocol == "https:" ? "https://" : "http://";
+    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init";
+    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+})(window, document, 'script', 'cloud.roistat.com', '9fcae1a19bf23011571b33710b6573d5');
+</script>
+
 @yield('scripts')
 </html>
