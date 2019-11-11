a383f1a7e41aac6711730bb04383fda79e68a277
root <root@potolki-ts.ru>
2019-11-11 18:32:12 +0300

    up gui

---
+++ resources/views/templates/gui/gui.blade.php
@@ -0,0 +1,17 @@
+@extends('templates.ceilings.layout')
+
+@section('content')
+	<main class="content gui">
+		<section class="container gui__typo">
+			<p class="overtitle">This is overtitle</p>
+			<h1 class="h1">h1 — This is a main title</h1>
+			<h2 class="h2">h2 — This is a subtitle</h2>
+			<h3 class="h3">h3 — This is a section title</h3>
+			<h4 class="h4">h4 — This is a Card Title</h4>
+			<h5 class="h5">h5 — Typography is the craft of endowing human language with a durable visual form.</h5>
+			<p class="intro">Intro — This an introduction text. It has a fixed size, and a custom line height, so you can experiment with it. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			<p class="text">Text — Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus vulputate diam eu pretium. Mauris elit orci, ultricies id fermentum vel, porta et eros. Vestibulum condimentum lectus in convallis feugiat. Sed vulputate fringilla felis. Aliquam ut arcu et dui feugiat scelerisque eu quis diam. Mauris placerat congue dui sit amet blandit. Phasellus condimentum libero vel velit auctor, sit amet tincidunt velit varius.</p>
+			<p class="text text_small">Text_small — Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus vulputate diam eu pretium. Mauris elit orci, ultricies id fermentum vel, porta et eros. Vestibulum condimentum lectus in convallis feugiat. Sed vulputate fringilla felis. Aliquam ut arcu et dui feugiat scelerisque eu quis diam. Mauris placerat congue dui sit amet blandit. Phasellus condimentum libero vel velit auctor, sit amet tincidunt velit varius.</p>
+		</section>
+	</main>
+@endsection
