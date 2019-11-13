a6cf70a7dde8ce88fafcbf6ab159484cf663f5ac
m.tsyganok <acces5denied@yandex.ru>
2019-11-13 14:22:25 +0300

    разделение направлений на кнопке вотсап

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1357,7 +1357,7 @@ $('.whatsapp_check').on('click', function (event) {
     var name = 'Пустой лид whatsapp';
     var city_code = [529, 523, 528, 113, 117];
 
-    if (window.city.bx_code in ity_code){
+    if (window.city.bx_code in city_code){
         name = 'WhatsApp - Запад';
     }else{
         name = 'WhatsApp - Восток';
