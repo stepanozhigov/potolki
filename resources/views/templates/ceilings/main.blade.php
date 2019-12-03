432823b37b54db34186eb98fb92a451e95c96c99
m.tsyganok <acces5denied@yandex.ru>
2019-12-03 17:49:02 +0300

    new temolate to route clone

--- resources/views/templates/ceilings/main.blade.php
+++ resources/views/templates/ceilings/main.blade.php
@@ -42,7 +42,7 @@
 			Нашли дешевле?<br>
 			<b>Сделаем скидку!</b>
 		</h2>
-		<img class="sale__img" src="/img/templates/gui/offer/opponent_sale.png" alt="Скидка до 500р от цены конкурента">
+		<img class="sale__img" src="/img/templates/gui/offer/opponent_sale.svg" alt="Скидка до 500р от цены конкурента">
 		<button data-title="Получить скидку" data-src="#popup_sale" class="buttonN buttonN-light sale__button js-show">Получить скидку</button>
     </section>
 
