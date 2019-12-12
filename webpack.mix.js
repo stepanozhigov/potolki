6c881375c03e437a9adad4953fad0400f7357cdb
m.tsyganok <acces5denied@yandex.ru>
2019-12-12 13:38:39 +0300

    add css-mqpacker

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,8 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .js('resources/assets/js/templates/ceilings/ceilings.js', 'public/js')
-   .sass('resources/assets/sass/templates/ceilings.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css').options({
+      postCss: [
+          require("css-mqpacker")
+      ]
+  });
