091cfde821e4b85df44775fc23e2497c819542a7
m.tsyganok <acces5denied@yandex.ru>
2019-12-23 12:57:23 +0300

    optiniz style

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -8,7 +8,7 @@
     <meta name="yandex-verification" content="6c7a8a6c99047a3c" />
     <meta name="google-site-verification" content="9PMBsqLABSxa0ShPlCJmBj0DCnuCrxL4v_kFdn2hakY" />
     <meta name="cmsmagazine" content="eaf22e35ae274666a6d7cdb9ea04fe37" />
-    
+    <link onload="if(media!='all') media='all'" rel="stylesheet" href="{{ Asset('/css/app.css') }}">
     <link rel="canonical" href="{{ url(Request::url()) }}" />
     <? $visit = new App\Visit; ?>
     @if (!empty($seoData))
@@ -90,7 +90,7 @@
         @yield('content')
     </div>
 </body>
-<link onload="if(media!='all') media='all'" rel="stylesheet" href="{{ Asset('/css/app.css') }}">
+
 <!-- <script>
         (function(w,d,u){
                 var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
