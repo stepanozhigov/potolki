c37dcac733a3528816f71b49ae6fff8fba27e691
m.tsyganok <acces5denied@yandex.ru>
2020-03-31 13:05:17 +0300

    calc

--- resources/assets/js/components/ceilingsCalcHome.vue
+++ resources/assets/js/components/ceilingsCalcHome.vue
@@ -2,30 +2,31 @@
     $(document).on('input', '#calc_home .range', function (e) {
         var area = $('[name=area]').val(),
             lamps = $('[name=lamps]').val(),
+            city = window.city.code,
             areaPrice = 350,
             lampPrice = 250,
             price = 0;
 
-        if (window.city.code == 'krasnodar') {
+        if (city === 'krasnodar') {
             areaPrice = 290;
         }
-        if (window.city.code == 'moskva' ||
-            window.city.code == 'dolgoprudnyj' ||
-            window.city.code == 'nahodka') {
+        if (city === 'moskva' ||
+            city === 'dolgoprudnyj' ||
+            city === 'nahodka') {
         	areaPrice = 450;
         }
 
-        if (window.city.code == 'ussuriysk' ||
-            window.city.code == 'vladivostok' ||
-            window.city.code == 'habarovsk' ||
-            window.city.code == 'blagoveschensk') {
+        if (city === 'ussuriysk' ||
+            city === 'vladivostok' ||
+            city === 'habarovsk' ||
+            city === 'blagoveschensk') {
         	areaPrice = 400;
         }
 
-        if (window.city.code == 'ussuriysk' ||
-            window.city.code == 'vladivostok' ||
-            window.city.code == 'habarovsk' ||
-            window.city.code == 'nahodka') {
+        if (city === 'ussuriysk' ||
+            city === 'vladivostok' ||
+            city === 'habarovsk' ||
+            city === 'nahodka') {
         	lampPrice = 300;
         }
 
