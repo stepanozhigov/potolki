9bccf06d52298540af90ab2f402c428590c0247b
m.tsyganok <acces5denied@yandex.ru>
2020-04-03 14:34:12 +0300

    h3

--- resources/assets/js/components/ceilingsCalcHome.vue
+++ resources/assets/js/components/ceilingsCalcHome.vue
@@ -2,19 +2,40 @@
     $(document).on('input', '#calc_home .range', function (e) {
         var area = $('[name=area]').val(),
             lamps = $('[name=lamps]').val(),
+            lampsPrice = 250,
             mult = 350,
             price = 0;
 
         if (window.city.code == 'krasnodar') {
             mult = 290;
         }
-        if (window.city.code == 'dolgoprudnyj') {
-        	mult = 450;
-        }
         if (window.city.code == 'moskva') {
             mult = 450;
         }
-        price = lamps * 250 + area * mult;
+        if (window.city.code == 'dolgoprudnyj') {
+        	mult = 450;
+        }
+        if (window.city.code == 'nahodka') {
+            mult = 450;
+            lampsPrice = 300;
+        }
+
+        if (window.city.code == 'ussuriysk') {
+            mult = 400;
+            lampsPrice = 300;
+        }
+        if (window.city.code == 'vladivostok') {
+            mult = 400;
+            lampsPrice = 300;
+        }
+        if (window.city.code == 'habarovsk') {
+            mult = 400;
+            lampsPrice = 300;
+        }
+        if (window.city.code == 'blagoveschensk') {
+            mult = 400;
+        }
+        price = lamps * lampsPrice + area * mult;
 
         $('.js-calc-price').html(price.toLocaleString());
     })
