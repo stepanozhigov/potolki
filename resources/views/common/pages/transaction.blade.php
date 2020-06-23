78b830c5cc66d9d3e27e34f8b7a32eee339b9d7e
m.tsyganok <acces5denied@yandex.ru>
2020-06-23 09:33:58 +0300

    payment

--- resources/views/common/pages/transaction.blade.php
+++ resources/views/common/pages/transaction.blade.php
@@ -34,17 +34,17 @@
 		</style>
 		<form class="payment__form" method="POST"  class="application"  accept-charset="UTF-8" action="https://partner.rficb.ru/alba/input/">
 		  <input type="hidden" name="key" value="yvgz56fJm6cDc6hpZJt1O5zcQQY6vVxdZaY1xCgk4OM=" />
-		  
-		  <!-- <input type="hidden" name="key" value="fvMXytFivKigwP8EzZ21ErVxb+PqIpiwt6RSDsmXwGQ=" /> -->
-		  <input type="hidden" name="name" value="potolki-ts.ru оплата договора"/>
-		  <input type="hidden" name="order_id" value="0" />
-		   <input class="input" name="fio" value="" placeholder="Ваше имя" />
-		   <input class="input" name="adres" value="" placeholder="Ваш адрес" />
-		   <input class="input" name="default_email" value="" placeholder="Ваш e-mail" />
-		   <input class="input" name="order" value="" placeholder="Номер договора" />
-		   <input class="input" name="cost" value="" placeholder="Сумма" />
 
-		  <button class="button" name="button">Оплатить</button>
+		    <!-- <input type="hidden" name="key" value="fvMXytFivKigwP8EzZ21ErVxb+PqIpiwt6RSDsmXwGQ=" /> -->
+		    <input type="hidden" name="name" value="potolki-ts.ru оплата договора"/>
+		    <input type="hidden" name="order_id" value="0" />
+            <input class="input" name="payer_name" value="" required placeholder="Ваше имя" />
+            <input class="input" name="address" value="" required placeholder="Ваш адрес" />
+            <input class="input" name="email" value="" required placeholder="Ваш e-mail" />
+            <input class="input" name="comment" value="" required placeholder="Номер договора" />
+            <input class="input" name="cost" value="" required placeholder="Сумма" />
+
+		    <button class="button" name="button">Оплатить</button>
 		</form>
 	</section>
 @endsection
